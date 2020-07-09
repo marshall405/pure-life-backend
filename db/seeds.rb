8014beaf7781a2a282cd5a77c0599cff99260630
iambryanhaney <i.am.bryan.haney@gmail.com>
2020-07-09 14:35:10 -0500

    added user roles

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,13 +7,24 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-user = User.create(email:'admin@purelife.com', first_name:'Admin', last_name:'Nimda', password: '123456', password_confirmation: '123456', is_admin: true)
+
+michael = Provider.create(first_name: "Michael", last_name: "Wilson")
+
+user = User.create(email:'admin@purelife.com', first_name:'Admin', last_name:'Nimda', 
+        password: '123456', password_confirmation: '123456', is_admin: true, role: 1, provider_id: michael.id)
 
 
-bryan = Provider.create(first_name: "Bryan", last_name: "Haney")
-sickly = Patient.create(first_name: "Sickly")
 
-bryan.appointments.create(patient_id: sickly.id)
+
+sickly = Patient.create(first_name: "Sickly", last_name: 'McSickFace')
+
+user = User.create(email: 'sickly@sickmail.com', first_name: 'Sickly', last_name: 'McSickFace', 
+        password: '123456', password_confirmation: '123456', is_admin: false, role: 0, patient_id: sickly.id)
+
+
+
+
+michael.appointments.create(patient_id: sickly.id)
 
 
 
