4190fca8f9a3aab64ffcbe30a1c15a6df44f1d96
iambryanhaney <i.am.bryan.haney@gmail.com>
2020-07-10 14:59:35 -0500

    patients see unavailable times for foreign reservations

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,16 +1,17 @@
 class AppointmentsController < ApplicationController
 
-    skip_before_action :authorize_request, only: [:create, :destroy]
+    skip_before_action :authorize_request, only: [:index, :create, :destroy]
+
+    def index
+        render json: Appointment.all, except: [:created_at, :updated_at]
+    end
     
     def create
-        print "\n\n\nIn the Appointments controller!\n\n\n"
-        print "Params: #{params}\n\n\n"
         appointment = Appointment.create(patient_id: params[:patient_id], provider_id: params[:provider_id], date: params[:date])
         render json: appointment, except: [:created_at, :updated_at]
     end
 
     def destroy
-        print "\n\n\nIn appointments#destroy\n\nParams: #{params}\n\n\n"
         appointment = Appointment.find_by(id: params[:id])
         appointment.destroy
         render json: appointment, except: [:created_at, :updated_at]
